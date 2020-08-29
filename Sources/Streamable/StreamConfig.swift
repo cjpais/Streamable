29ce88638bf9cc0a15f0736b47218841694a6012
CJ Pais <cj@cjpais.com>
2020-08-29 12:58:47 -0700

    inital code for streamable

---
+++ Sources/Streamable/StreamConfig.swift
@@ -0,0 +1,14 @@
+//
+//  StreamConfig.swift
+//  
+//
+//  Created by CJ Pais on 8/29/20.
+//
+
+import Foundation
+
+public struct StreamConfig: Encodable {
+    var namespace: String
+    var name: String
+    var version: String
+}
--- Sources/Streamable/Streamable.swift
+++ Sources/Streamable/Streamable.swift
@@ -1,3 +1,39 @@
-struct Streamable {
-    var text = "Hello, World!"
+//
+//  Streamable.swift
+//
+//
+//  Created by CJ Pais on 8/29/20.
+//
+
+import Foundation
+
+struct Streamable<T: Encodable>: Encodable {
+    var config: StreamConfig
+    var data: T
+    
+    public func sendStream(to baseURL: String) {
+        let encoder = JSONEncoder()
+        do {
+            let data = try encoder.encode(self)
+            if let url = URL(string: baseURL + "/stream") {
+                var request = URLRequest(url: url)
+                
+                request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+                request.httpMethod = "POST"
+                request.httpBody = data
+                
+                let dataTask = URLSession.shared.dataTask(with: request) { data, response, error in
+                    if error != nil {
+                        print(error!)
+                    }
+                }
+                
+                dataTask.resume()
+            } else {
+                print("URL couldn't be created")
+            }
+        } catch {
+            print(error)
+        }
+    }
 }
