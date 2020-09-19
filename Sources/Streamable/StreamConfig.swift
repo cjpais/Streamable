8b25482d0a6f4090ea716492a8e5263895136c6b
CJ Pais <cj@cjpais.com>
2020-09-19 12:24:04 -0700

    add UUID to streamable

--- Sources/Streamable/StreamConfig.swift
+++ Sources/Streamable/StreamConfig.swift
@@ -11,10 +11,12 @@ public struct StreamConfig: Encodable {
     var namespace: String
     var name: String
     var version: String
+    var uuid: UUID
     
-    public init(namespace: String, name: String, version: String) {
+    public init(namespace: String, name: String, version: String, uuid: UUID) {
         self.namespace = namespace
         self.name = name
         self.version = version
+        self.uuid = uuid
     }
 }
