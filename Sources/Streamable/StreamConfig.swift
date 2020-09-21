553d52a9b405ff5bbcf62ed40dd49b769f6d0abf
CJ Pais <cj@cjpais.com>
2020-09-21 15:59:58 -0700

    added location into struct definition and additional test for duplicate entity going to DB

--- Sources/Streamable/StreamConfig.swift
+++ Sources/Streamable/StreamConfig.swift
@@ -6,17 +6,25 @@
 //
 
 import Foundation
+import CoreLocation
+
+public struct Point: Encodable {
+    var lat: Double
+    var long: Double
+}
 
 public struct StreamConfig: Encodable {
     var namespace: String
     var name: String
     var version: String
     var uuid: UUID
+    var location: Point?
     
-    public init(namespace: String, name: String, version: String, uuid: UUID) {
+    public init(namespace: String, name: String, version: String, uuid: UUID, location: Point?) {
         self.namespace = namespace
         self.name = name
         self.version = version
         self.uuid = uuid
+        self.location = location
     }
 }
--- Tests/StreamableTests/StreamableTests.swift
+++ Tests/StreamableTests/StreamableTests.swift
@@ -4,14 +4,14 @@ import XCTest
 class StreamableTests: XCTestCase {
     
     func testStreamConfigInit() {
-        let config = StreamConfig(namespace: "test", name: "test", version: "0.0.1", uuid: UUID())
+        let config = StreamConfig(namespace: "test", name: "test", version: "0.0.1", uuid: UUID(), location: nil)
         XCTAssertEqual(config.namespace, "test")
         XCTAssertEqual(config.name, "test")
         XCTAssertEqual(config.version, "0.0.1")
     }
     
     func testStreamableDataInit() {
-        let config = StreamConfig(namespace: "test", name: "test", version: "0.0.1", uuid: UUID())
+        let config = StreamConfig(namespace: "test", name: "test", version: "0.0.1", uuid: UUID(), location: nil)
         let streamable = StreamableData<String>(config: config, data: "test data")
         XCTAssertEqual(streamable.config.namespace, config.namespace)
         XCTAssertEqual(streamable.data, "test data")
@@ -22,7 +22,14 @@ class StreamableTests: XCTestCase {
     }
     
     func testStreamableDataSendTest() {
-        let config = StreamConfig(namespace: "cj/test", name: "streamableLibrary", version: "0.0.1", uuid: UUID())
+        let config = StreamConfig(namespace: "cj/test", name: "streamableLibrary", version: "0.0.1", uuid: UUID(), location: Point(lat: 23.1231, long: -34.123))
+        let streamable = StreamableData<String>(config: config, data: "test data")
+        streamable.sendStream(to: "http://192.168.1.130:10000/dev", completionHandler: checkError)
+        sleep(1)
+    }
+    
+    func testExistingUUID() {
+        let config = StreamConfig(namespace: "cj/test", name: "streamableLibrary", version: "0.0.1", uuid: UUID(uuidString: "e6a20b77-b4ec-4f79-b211-e93209714264")!, location: Point(lat: 23.1231, long: -34.123))
         let streamable = StreamableData<String>(config: config, data: "test data")
         streamable.sendStream(to: "http://192.168.1.130:10000/dev", completionHandler: checkError)
         sleep(1)
